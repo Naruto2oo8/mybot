09afbd1150e9afd3aaa6fbb45f42a03f71510b2a
Naruto2oo8 <muhammdrasul20@gmail.com>
2025-07-20 19:16:55 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,11 @@
-a
+import telebot
+import os
+
+TOKEN = os.getenv("8130245687:AAGZBBsnJuty9_Bfbq9gFsYUdyGReGVf7dw")
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.send_message(message.chat.id, "Salom, Anime bot ishga tushdi!")
+
+bot.infinity_polling()
